901d03c779e9026aab2cb0c1aaa3310133a3533f
Mark Veltzer <mark.veltzer@gmail.com>
2021-11-01 16:43:52 +0200

    small fix

--- exercises/04_command_line_import_and_export/delete_all.sh
+++ exercises/04_command_line_import_and_export/delete_all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl http://localhost:8983/solr/my_core/update -H "Content-type: text/xml" --data-binary '<delete><query>*:*</query></delete>'
-curl http://localhost:8983/solr/my_core/update -H "Content-type: text/xml" --data-binary '<commit />'
+curl http://localhost:8983/solr/my_core/update -H "Content-type: text/xml" --data-binary "<delete><query>*:*</query></delete>"
+curl http://localhost:8983/solr/my_core/update -H "Content-type: text/xml" --data-binary "<commit />"
