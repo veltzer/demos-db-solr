b61f073c2a8649ce6004c7af81983bb1cdd0f34d
Mark Veltzer <mark.veltzer@gmail.com>
2021-11-08 08:05:18 +0200

    more improvements

--- exercises/09_create_config_set_and_core/do_all.sh
+++ exercises/09_create_config_set_and_core/do_all.sh
@@ -1,10 +1,22 @@
 #!/bin/bash -e
 SOLR_HOME="$HOME/install/solr"
-(cd $SOLR_HOME/server/solr/configsets/sample_techproducts_configs/conf && zip -qr - *) > myconfigset.zip
-if ! curl -s -X DELETE "http://localhost:8983/api/cluster/configs/myConfigSet?omitHeader=true" > /dev/null
+if curl -s -X DELETE "http://localhost:8983/api/cluster/configs/myConfigSet?omitHeader=true" > /dev/null
 then
-	echo "did not delete previous copy"
+	echo "deleted myConfigSet"
+else
+	echo "did not delete myConfigSet"
 fi
+if ! curl -s -X DELETE "http://localhost:8983/api/cluster/configs/myConfigSetCopy?omitHeader=true" > /dev/null
+then
+	echo "deleted myConfigSetCopy"
+else
+	echo "did not delete myConfigSetCopy"
+fi
+# list all configsets
+curl -s "http://localhost:8983/api/cluster/configs?omitHeader=true"
+
+(cd $SOLR_HOME/server/solr/configsets/sample_techproducts_configs/conf && zip -qr - *) > myconfigset.zip
+
 curl -s -X POST --header "Content-Type:application/octet-stream" --data-binary @myconfigset.zip "http://localhost:8983/solr/admin/configs?action=UPLOAD&name=myConfigSet"
 curl -s -X POST -H 'Content-type: application/json' -d '{
 	"create": {
@@ -12,3 +24,11 @@ curl -s -X POST -H 'Content-type: application/json' -d '{
 		"baseConfigSet": "myConfigSet"
 	}
 }' 'http://localhost:8983/api/cluster/configs?omitHeader=true'
+
+# list all configsets
+curl -s "http://localhost:8983/api/cluster/configs?omitHeader=true"
+
+curl -s -X DELETE "http://localhost:8983/api/cluster/configs/myConfigSetCopy?omitHeader=true" > /dev/null
+
+# list all configsets
+curl -s "http://localhost:8983/api/cluster/configs?omitHeader=true"
