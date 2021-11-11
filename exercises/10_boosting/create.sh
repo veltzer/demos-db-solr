f1937d6ea061d4d248bb090ddb2bf2eb0a9915c0
Mark Veltzer <mark.veltzer@gmail.com>
2021-11-11 08:24:39 +0200

    more stuff

--- exercises/10_boosting/create.sh
+++ exercises/10_boosting/create.sh
@@ -1,7 +1,7 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 SOLR_HOME=$HOME/install/solr
 $SOLR_HOME/bin/solr create -c films
-$SOLR_HOME/bin/solr config -c films -action set-user-property -property update.autoCreateFields -value false
+# $SOLR_HOME/bin/solr config -c films -action set-user-property -property update.autoCreateFields -value false
 curl http://localhost:8983/solr/films/schema -X POST -H 'Content-type:application/json' --data-binary '{
     "add-field" : {
         "name":"name",
